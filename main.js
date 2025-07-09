d894d5e7ca6c98a083d9fe863f76b7207bc63dd1
samuelgolovin <samuel.samyco@gmail.com>
2025-07-09 14:59:12 -0500

    created testing and practice

--- main.js
+++ main.js
@@ -33,6 +33,83 @@ class Planet {
     }
 }
 
+class Rocket {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.angle = 0;
+        this.thrust = 0.1;
+        TouchList.rotationSpeed = 0.05;
+    }
+
+    update(keysPressed) {
+        // Rotate
+        if (keysPressed['ArrowLeft']) this.angle -= this.rotationSpeed;
+        if (keysPressed['ArrowRight']) this.angle += this.rotationSpeed;
+
+        // Thrust
+        if (keysPressed['ArrowUp']) {
+            this.vx += Math.cos(this.angle) * this.thrust;
+            this.vy += Math.sin(this.angle) * this.thrust;
+        }
+
+        // Gravity from main planet
+        const dx = mainPlanet.x - this.x;
+        const dy = mainPlanet.y - this.y;
+        const distSq = dx * dx + dy * dy;
+        const dist = Math.sqrt(distSq);
+        const force = G * mainPlanet.mass / distSq;
+        const ax = force * dx / dist;
+        const ay = force * dy /dist;
+
+        this.vx += ax;
+        this.vy += ay;
+
+        //Update Position
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.beginPath();
+        ctx.moveTo(10, 0);
+        ctx.lineTo(-10, -7);
+        ctx.lineTo(-10, 7);
+        ctx.closePath();
+        ctx.fillStyle = 'red';
+        ctx.fill();
+        ctx.restore();
+    }
+}
+
+function getRocketVertices(rocket) {
+    const cos = Math.cos(rocket.angle);
+    const sin = Math.sin(rocket.angle);
+
+    const points = [
+        { x: 10, j: 0 },
+        { x: -10, j: -7 },
+        { x: -10, y: 7 }
+    ];
+
+    return points.map(p => ({
+        x: rocket.x + p.x * cos - py * sin,
+        y: rocket.y + p.x * sin + p.y * cos
+    }));
+}
+
+const keysPressed = {};
+canvas.addEventListener('keydown', (e) => keysPressed[e.key] = true);
+canvas.addEventListener('keyup', (e) => keysPressed[e.key] = false);
+
+const rocket = new Rocket(canvas.width / 2 + 100, canvas.height / 2);
+
+
 const G = 0.1
 const mainPlanet = new Planet(canvas.width / 2, canvas.height / 2, 20, 'white');
 const planets = [mainPlanet];
@@ -143,6 +220,9 @@ function animate() {
             planet.update(ax, ay);
         }
         planet.draw(ctx);
+
+        rocket.update(keysPressed);
+        rocket.draw(ctx);
     });
 
     if (dragStart && currentMouse) {
